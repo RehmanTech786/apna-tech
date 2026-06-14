646285c335a0e5d65cc87706324feeb9ecf61f0f
Rehman <rtech78610@gmail.com>
2026-06-14 14:35:02 +0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@
 function App  () {
   return (
 
-    <div className="text-amber-400" >Welcome to MY Website</div>
+    <div className="text-amber-400" >
+    <p>helo</p>
+  
+    
+     </div>
   )
 }
 
